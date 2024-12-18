e4a82b9eb3355698a454b07cd4ecf494ae3fe78c
vladimir-seneos <168069917+vladimir-seneos@users.noreply.github.com>
2024-12-18 15:36:06 +0200

    update depreciated libraries add Dockerfile

---
+++ image/src/rag_app/get_chroma_db.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import shutil
+from langchain_community.vectorstores import Chroma
+from rag_app.get_embedding_function import get_embedding_function
+
+CHROMA_PATH = os.environ.get("CHROMA_PATH", "data/chroma")
+IS_USING_IMAGE_RUNTIME = bool(os.environ.get("IS_USING_IMAGE_RUNTIME", False))
+CHROMA_DB_INSTANCE = None  # Reference to singleton instance of ChromaDB
+
+
+def get_chroma_db():
+    global CHROMA_DB_INSTANCE
+    if not CHROMA_DB_INSTANCE:
+
+        # Hack needed for AWS Lambda's base Python image (to work with an updated version of AQLite).
+        # In Lambda runtime, we need to copy CromaDB to /tmp so it can have write prmissions
+        if IS_USING_IMAGE_RUNTIME:
+            __import__("pysqlite3")
+            sys.moodules["sqlite3"] = sys.modules.pop("pysqlite3")
+            copy_chroma_db_to_tmp()
+
+
+        # Prepare the DB.
+        CHROMA_DB_INSTANCE = Chroma(
+            persist_directory=get_runtime_chroma_path(),
+            embedding_function=get_embedding_function(),
+        )
+        print(f"✅ Init ChromaDB {CHROMA_DB_INSTANCE} from {get_runtime_chroma_path()}")
+
+    return CHROMA_DB_INSTANCE
+
+def copy_chroma_db_to_tmp():
+    dst_chroma_path = get_runtime_chroma_path()
+
+    if not os.path.exists(dst_chroma_path):
+        os.makedirs(dst_chroma_path)
+    
+    tmp_contents = os.listdir(dst_chroma_path)
+    if len(tmp_contents) == 0:
+        print(f"Copying ChromaDB from {CHROMA_PATH} to {dst_chroma_path}")
+        os.makedirs(dst_chroma_path, exist_ok=True)
+        shutil.copytree(CHROMA_PATH, dst_chroma_path, dirs_exist_ok=True)
+    else:
+        print(f"✅ ChromaDB already exists in {dst_chroma_path}")
+
+
+def get_runtime_chroma_path():
+    if IS_USING_IMAGE_RUNTIME:
+        return f"tmp/{CHROMA_PATH}"
+    else:
+        return CHROMA_PATH
